5d84224129ad19f53e146502aab9e16225387a8d
AdrielAnselmo <adriel-ac@hotmail.com>
2024-02-15 22:32:45 -0300

    refactor: Remove arquivos desnecessários

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {A pp} from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
